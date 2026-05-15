4083f6bb3942a882badfbe3e642c6286811784ff
desumn <ilyess.tahar@posteo.net>
2026-05-15 20:53:16 +0200

    add a basic location module

---
+++ lib/parsing/location.ml
@@ -0,0 +1,12 @@
+
+type t = { start : Lexing.position; end_ : Lexing.position;  }
+
+let make start end_ = {start; end_}
+
+let from_lexbuf (lexbuf : Lexing.lexbuf) = make lexbuf.lex_start_p lexbuf.lex_curr_p
+
+let pp ppf loc =
+  Fmt.pf ppf "%s: %d:%d-%d:%d"
+    loc.start.pos_fname 
+    loc.start.pos_lnum loc.start.pos_cnum
+    loc.end_.pos_lnum loc.start.pos_cnum
---
+++ lib/parsing/location.mli
@@ -0,0 +1,9 @@
+type t = private { start : Lexing.position; end_ : Lexing.position;  }
+
+val make : Lexing.position -> Lexing.position -> t
+
+val from_lexbuf : Lexing.lexbuf -> t
+
+val pp : t Fmt.t
+
+
